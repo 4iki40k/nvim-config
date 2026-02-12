afc2cd50297c54229b8c438fe9a3992bb243e0f9
4iki40k <4iki40k@gmail.com>
2026-02-12 22:26:23 +0200

    fixed tresitter
    treesitter must be from master branch, not from main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,12 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
+  version = false,
+  branch = 'master',
   build = ':TSUpdate',
  config = function()
-   local config = require("nvim-treesitter.configs")
-    config.setup({
+   local configs = require("nvim-treesitter.configs")
+    configs.setup({
       ensure_installed = {"lua", "asm", "bash", "c", "cpp", "cmake", "python", "sql"},
       highlight = {enable = true},
       indent = {enable = true}
