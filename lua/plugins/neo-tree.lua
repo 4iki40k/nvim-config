6de09fa4984b668fefe8151a45dd2dc742fd8521
Андрій <85013313+4iki40k@users.noreply.github.com>
2026-02-13 05:44:31 +0200

    Merge pull request #3 from 4iki40k/small-fixes-and-additions
    small fixes and additions

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,7 +8,8 @@ return {
     },
     lazy = false, -- neo-tree will laziiily load itself
     config = function()
-      vim.keymap.set('n', '<leader>t', ':Neotree filesystem reveal left<CR>', {})
+      vim.keymap.set('n', '<leader>to', ':Neotree filesystem reveal left<CR>', {})
+      vim.keymap.set('n', '<leader>tc', ':Neotree filesystem close<CR>', {})
       vim.api.nvim_create_autocmd("VimEnter", {
         callback = function()
           vim.cmd("Neotree filesystem reveal left")
