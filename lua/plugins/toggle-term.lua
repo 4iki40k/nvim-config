a16eefdd3cc0ebb9bff323e6565f2940b04bf057
4iki40k <4iki40k@gmail.com>
2026-02-13 05:29:37 +0200

    small fixes and additions
    added keymap for neotree clos filesystem
    added rows numbers

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -5,8 +5,7 @@ return {
     vim.keymap.set('n', '<leader>tf', ':ToggleTerm direction=float <enter>', {silent = true})
     vim.keymap.set('n', '<leader>th', ':ToggleTerm direction=horizontal <enter>', {silent = true})
     function _G.set_terminal_keymaps()
-      local opts = {buffer = 0}
-      vim.keymap.set('t', '<A-Esc>', [[<C-\><C-n>]], opts)
+      vim.keymap.set('t', '<A-Esc>', [[<C-\><C-n>]], {buffer = 0})
     end
     vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
   end
